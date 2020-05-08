66ecb40bb4fcfec1e5b196b1714538a596cc924a
Eurelian <44678944+Eurelian@users.noreply.github.com>
2020-05-08 18:13:04 +0200

    Updates

---
+++ precache-manifest.19a9b006129d099f6528920db20eb64a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecac75b5878ade2313a043ea47ece38a",
+    "url": "/yelpME/index.html"
+  },
+  {
+    "revision": "a6dca5e54c858261f2b0",
+    "url": "/yelpME/static/css/main.15bb9389.chunk.css"
+  },
+  {
+    "revision": "58ad88b915516cd25865",
+    "url": "/yelpME/static/js/2.ebfbe558.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/yelpME/static/js/2.ebfbe558.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6dca5e54c858261f2b0",
+    "url": "/yelpME/static/js/main.c6ce7866.chunk.js"
+  },
+  {
+    "revision": "3092a7b357d69558b6b7",
+    "url": "/yelpME/static/js/runtime-main.2394a3e2.js"
+  },
+  {
+    "revision": "16c691a12608b128b973504aaf727d45",
+    "url": "/yelpME/static/media/berlin.16c691a1.png"
+  }
+]);
